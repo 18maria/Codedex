560284cdf145b162f06f9f814c57e69462018f16
18maria <122528031+18maria@users.noreply.github.com>
2023-02-01 09:29:55 +0100

    Add files via upload

---
+++ Ejer4_2.py
@@ -0,0 +1,11 @@
+numero1=int(input("Dame una numero real"))
+if numero1 == 0:
+    print("es numero es cero")
+elif numero1 >= 1:
+    print("el numero es positivo")
+elif numero1 <0:
+    print("el numero es negativo")
+
+
+
+
