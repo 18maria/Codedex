31041bff8199a7e072b4b369d47eb45615bef363
18maria <122528031+18maria@users.noreply.github.com>
2023-02-02 12:00:40 +0100

    Add files via upload

---
+++ enter_pin.py
@@ -0,0 +1,18 @@
+print('Banco de Códedex')
+contraseña = int(input('Pon tu pin: '))
+while contraseña !=1111:
+    contraseña = int(input('Pin incorreto. Pon tu pin otra vez: '))
+if contraseña== 1111:
+    print("Pin correcto")
+while contraseña !=2023:
+    contraseña = int(input('Pin incorreto. Pon tu pin otra vez: '))
+if contraseña== 2023:
+    print("Pin correcto")
+while contraseña !=1991:
+    contraseña = int(input('Pin incorreto. Pon tu pin otra vez: '))
+if contraseña==1991:
+    print("Pin correcto")
+if contraseña== 1234:
+    print("Pin correcto")
+while contraseña !=1234:
+    contraseña = int(input('Pin incorreto. Pon tu pin otra vez: '))
