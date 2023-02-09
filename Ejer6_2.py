a41651985f04806e67024e47e4f16fb917dcafd3
18maria <122528031+18maria@users.noreply.github.com>
2023-02-09 11:57:19 +0100

    Add files via upload

---
+++ Ejer6_2.py
@@ -0,0 +1,8 @@
+user=input("Dame el usuario admin ")
+contrasenia=input("Dame la contraseña 0987 ")
+while user != "admin" or contrasenia!="0987":
+    print("ERROR")
+    user=input("Dame el usuario")
+    contrasenia=input("Dame la contraseña admin")
+
+print("Bienvenido al sistema")
